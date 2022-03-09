6a6c2162519b8f2d9508f2b93aa2943c0ad173d3
Mawi1e <sw031489@naver.com>
2022-03-09 21:58:34 +0900

    DirectX12Study

---
+++ 2022-03-04~2022-03-09/sources/FrameResource.cpp
@@ -0,0 +1,16 @@
+#include "FrameResource.h"
+
+namespace Mawi1e {
+	FrameResource::FrameResource(ID3D12Device* Device, UINT passCount, UINT objCount, UINT matCount, UINT waveVertCount) {
+		Device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT,
+			IID_PPV_ARGS(m_CommandAllocator.GetAddressOf()));
+
+		m_ObjCB = std::make_unique<UploadBuffer<ObjConstants>>(Device, objCount, true);
+		m_PassCB = std::make_unique<UploadBuffer<PassConstants>>(Device, passCount, true);
+		m_MatCB = std::make_unique<UploadBuffer<MaterialConstants>>(Device, matCount, true);
+		m_WaveVB = std::make_unique<UploadBuffer<Vertex>>(Device, waveVertCount, false);
+	}
+
+	FrameResource::~FrameResource() {
+	}
+}
